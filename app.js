30f850fabfce60d2cf5ef2d3f5607d2c3ffb9fad
VladimirCharkot <vlad.chk@gmail.com>
2021-09-29 14:52:47 -0300

    Quitamos video de portada en mobile

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var sassMiddleware = require('node-sass-middleware')
-
+let useragent = require('express-useragent')
 var indexRouter = require('./routes/index')
 
 var app = express()
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -24,6 +26,8 @@ app.use(sassMiddleware({
   sourceMap: true
 }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(useragent.express())
 
 app.use('/', indexRouter)
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var express = require('express')
-var router = express.Router()
+let express = require('express')
+let router = express.Router()
 
-router.get('/', (req, res, next) => res.render('index', { title: 'CAM22' }))
+
+router.get('/', (req, res, next) => res.render('index', { title: 'CAM22', celu: req.useragent.isMobile }))
 router.get('/historia', (req, res, next) => res.render('historia', { title: 'CAM22 — Historia' }))
 router.get('/orga', (req, res, next) => res.render('orga', { title: 'CAM22 — Organización' }))
 
