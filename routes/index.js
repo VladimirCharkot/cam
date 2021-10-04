847642de1ba92a772dcf54bbaf0604934a16738f
VladimirCharkot <vlad.chk@gmail.com>
2021-10-04 01:08:10 -0300

    +galería

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 let express = require('express')
+let _ = require('lodash')
 let router = express.Router()
 
 
 router.get('/', (req, res, next) => res.render('index', { title: 'CAM22', celu: req.useragent.isMobile }))
-router.get('/historia', (req, res, next) => res.render('historia', { title: 'CAM22 — Historia' }))
+router.get('/historia', async (req, res, next) => res.render('historia', { title: 'CAM22 — Historia', galeria: _.chunk(_.range(72), 72/3)}))
 router.get('/orga', (req, res, next) => res.render('orga', { title: 'CAM22 — Organización' }))
 
 module.exports = router
